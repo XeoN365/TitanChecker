a3b5ce5a416f07c6fef06eecef8dc75b37086bc8
XeoN365 <xee3oon@gmail.com>
2022-09-28 21:53:43 +0100

    First commit

---
+++ Start Checker.ps1
@@ -0,0 +1,3 @@
+env/scripts/activate.ps1
+py checker.py
+Read-Host -Prompt "Press Enter to exit"
---
+++ launcher.py
@@ -0,0 +1,26 @@
+from checker import Checker
+import logging
+import git
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Launcher():
+    def __init__(self):
+        self.checker = Checker()
+        self.USERNAME = os.getenv('USERNAME')
+        self.TOKEN = os.getenv('TOKEN')
+        self.giturl = f"https://{self.USERNAME}:{self.TOKEN}@github.com/XeoN365/TitanChecker.git"
+    
+    def update(self):
+        g = git.Git(self.giturl)
+        g.pull('master', 'main')
+        
+        
+        
+
+if __name__ == "__main__":
+    launcher = Launcher()
+    launcher.update()
+    launcher.checker.start()
