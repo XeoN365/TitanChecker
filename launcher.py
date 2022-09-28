45ba89d4b23a200f400018114bb67d94b270f43d
XeoN365 <xee3oon@gmail.com>
2022-09-28 22:20:43 +0100

    updated launcher

--- launcher.py
+++ launcher.py
@@ -12,14 +12,19 @@ class Launcher():
         self.USERNAME = os.getenv('USERNAME')
         self.TOKEN = os.getenv('TOKEN')
         self.giturl = f"https://{self.USERNAME}:{self.TOKEN}@github.com/XeoN365/TitanChecker.git"
+        self.logging = logging.getLogger("Launcher")
     
     def update(self):
-        g = git.Git(self.giturl)
-        logging.info("Starting update!")
-        g.pull('master', 'main')
-        logging.info("Update finished!")
+        try:
+            repo = git.Repo.clone_from(self.giturl, os.getcwd())
+        except Exception as e:
+            repo = git.Repo(os.getcwd())
+        q = repo.remotes.master
+        q.pull()
+        print("done")
         
 if __name__ == "__main__":
     launcher = Launcher()
+    
     launcher.update()
-    launcher.checker.start()
+    #launcher.checker.start()
